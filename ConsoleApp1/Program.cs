d71da46e8ff74b6bc25653c81e18c467a34877fc
Agnese <fodza@inbox.lv>
2018-05-07 20:01:26 +0300

    2.dienas
    1.uzd. 2.dienai - ievadi dzimsanas datus

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Kad esi dzimis?");//izvade
+
+            String dati; // pieskirt simbolu virknei definiciju
+            dati = Console.ReadLine(); //uz ekrana ieraksta dzimsanas gadu
+
+            Console.WriteLine("Jus esat dzimis " + dati);//izvada tekstu(rezultatu) + to, kas tika ievadits uz ekrana zem "dati" 
+
+            Console.ReadLine();
+                        
+        }
+    }
+}
